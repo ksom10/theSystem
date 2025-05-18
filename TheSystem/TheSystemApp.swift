e11313a2b7503acd38b9164a6efefed879ad4ddb
ksom10 <someson11@aol.com>
2025-05-18 17:19:07 -0500

    Initial Commit

---
+++ TheSystem/TheSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheSystemApp.swift
+//  TheSystem
+//
+//  Created by Kameron Someson on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct TheSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
